b55a11fedad449dbbf28f87a8fcb45f6eef04d55
Cmalonzo1 <142922371+Cmalonzo1@users.noreply.github.com>
2024-09-27 18:11:40 +0000

    created assignment 6

--- Homepage/assets/js/plusOperator.js
+++
@@ -1,9 +0,0 @@
-function addTwoNumbers(){
-    let numOne = Math.random() * 9;
-    let numTwo = Math.random() * 9;
-    let sum = Math.round(numOne + numTwo);
-    
-    return sum;
-}
-
-console.log(addTwoNumbers());
